028d232ef0dd2a4dcd7f4f9d9e48a4e457deb229
unknown <Akib_Rayhan>
2023-04-12 20:25:32 +0600

    update

--- js/app.js
+++ js/app.js
@@ -46,9 +46,7 @@ for (let i = 0; i < itc_chart_item.length; i++) {
       div.classList.add("itc_chart_indicator");
       div.innerHTML = `<li><div>${item}</div>
       <div>
-        <span >
-        <button id="open-modal-btn">Open Modal</button>
-                  
+        <span onclick='modalOpen()'>
         <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="white" class="bi bi-gear" viewBox="0 0 16 16">
           <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
           <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
@@ -58,7 +56,45 @@ for (let i = 0; i < itc_chart_item.length; i++) {
         <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg height="24px" id="Layer_1" fill='white' style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="24px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M256,128c-81.9,0-145.7,48.8-224,128c67.4,67.7,124,128,224,128c99.9,0,173.4-76.4,224-126.6   C428.2,198.6,354.8,128,256,128z M256,347.3c-49.4,0-89.6-41-89.6-91.3c0-50.4,40.2-91.3,89.6-91.3s89.6,41,89.6,91.3   C345.6,306.4,305.4,347.3,256,347.3z"/><g><path d="M256,224c0-7.9,2.9-15.1,7.6-20.7c-2.5-0.4-5-0.6-7.6-0.6c-28.8,0-52.3,23.9-52.3,53.3c0,29.4,23.5,53.3,52.3,53.3    s52.3-23.9,52.3-53.3c0-2.3-0.2-4.6-0.4-6.9c-5.5,4.3-12.3,6.9-19.8,6.9C270.3,256,256,241.7,256,224z"/></g></g></svg>
         </span>
         <span class="itc_chart_delete_icon"><?xml version="1.0" ?><svg viewBox="0 0 512 512" height="24px" width="24px" fill='white' xmlns="http://www.w3.org/2000/svg"><title/><g data-name="1" id="_1"><path d="M356.65,450H171.47a41,41,0,0,1-40.9-40.9V120.66a15,15,0,0,1,15-15h237a15,15,0,0,1,15,15V409.1A41,41,0,0,1,356.65,450ZM160.57,135.66V409.1a10.91,10.91,0,0,0,10.9,10.9H356.65a10.91,10.91,0,0,0,10.91-10.9V135.66Z"/><path d="M327.06,135.66h-126a15,15,0,0,1-15-15V93.4A44.79,44.79,0,0,1,230.8,48.67h66.52A44.79,44.79,0,0,1,342.06,93.4v27.26A15,15,0,0,1,327.06,135.66Zm-111-30h96V93.4a14.75,14.75,0,0,0-14.74-14.73H230.8A14.75,14.75,0,0,0,216.07,93.4Z"/><path d="M264.06,392.58a15,15,0,0,1-15-15V178.09a15,15,0,1,1,30,0V377.58A15,15,0,0,1,264.06,392.58Z"/><path d="M209.9,392.58a15,15,0,0,1-15-15V178.09a15,15,0,0,1,30,0V377.58A15,15,0,0,1,209.9,392.58Z"/><path d="M318.23,392.58a15,15,0,0,1-15-15V178.09a15,15,0,0,1,30,0V377.58A15,15,0,0,1,318.23,392.58Z"/><path d="M405.81,135.66H122.32a15,15,0,0,1,0-30H405.81a15,15,0,0,1,0,30Z"/></g></svg></span>
-      </div></li>`;
+      </div></li>
+      
+      <div id="modal" class="modal">
+        <div class="modal-content">
+          <div class="modal-header">
+            <span class="close-btn">&times;</span>
+            <h4>${item}</h4>
+          </div>
+          <div class="modal-body">
+            <p>
+              <strong>Main settings</strong>
+            </p>
+            <div style="display: flex; justify-content: space-between">
+              <div>
+                <p>Period</p>
+                <input type="text" value="14" name="period" />
+              </div>
+              <div>
+                <p>Color</p>
+                <div>
+                  <div>
+                    <div id="color-selected"></div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p>Thinness</p>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button id="reset-btn">Reset</button>
+            <button id="cancel-btn">Cancel</button>
+            <button id="apply-btn">Apply</button>
+          </div>
+        </div>
+      </div>
+
+      `;
       itc_chart_container.appendChild(div);
       const deleteIcon = div.querySelector(".itc_chart_delete_icon");
       deleteIcon.addEventListener("click", function () {
@@ -74,18 +110,18 @@ for (let i = 0; i < itc_chart_item.length; i++) {
 
 // Modal JS
 const modal = document.getElementById("modal");
-const openModalBtn = document.getElementById("open-modal-btn");
+// const openModalBtn = document.getElementById("open-modal-btn");
 const closeBtn = document.querySelector(".close-btn");
 const resetBtn = document.getElementById("reset-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const applyBtn = document.getElementById("apply-btn");
 
-openModalBtn.addEventListener("click", function () {
-  modal.style.display = "block";
-});
+// openModalBtn.addEventListener("click", function () {
+//   modal.style.display = "block";
+// });
 
 closeBtn.addEventListener("click", function () {
-  modal.style.display = "none";
+  document.getElementById("modal").style.display = "none";
 });
 
 resetBtn.addEventListener("click", function () {
@@ -93,10 +129,14 @@ resetBtn.addEventListener("click", function () {
 });
 
 cancelBtn.addEventListener("click", function () {
-  modal.style.display = "none";
+  document.getElementById("modal").style.display = "none";
 });
 
 applyBtn.addEventListener("click", function () {
   console.log("Apply button clicked");
 });
+
+function modalOpen() {
+  document.getElementById("modal").style.display = "block";
+}
 
